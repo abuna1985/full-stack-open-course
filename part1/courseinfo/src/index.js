72465979699a163b5c0f4b1650f57a0c3d59e7d8
adamjabundis@gmail.com <adamjabundis@gmail.com>
2020-12-29 21:55:39 -0800

    Solution for 1.3: course information step3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -8,8 +8,8 @@ const Header = ({course}) => {
 }
 
 // Part will have the text for part and exercise number
-const Part = ({part, exercises}) => {
-  return <p>{part} {exercises}</p>;
+const Part = ({name, exercises}) => {
+  return <p>{name} {exercises}</p>;
 }
 
 // Create a content
@@ -32,23 +32,29 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course} />
       {/* Decided to pass through an object for each part */}
       <Content 
-        part1={{part: part1, exercises: exercises1}} 
-        part2={{part: part2, exercises: exercises2}} 
-        part3={{part: part3, exercises: exercises3}}
+        part1={part1} 
+        part2={part2} 
+        part3={part3}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   );
 }
