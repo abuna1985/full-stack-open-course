6db0e64548ea264a69967d3cd62837028ad78374
adamjabundis@gmail.com <adamjabundis@gmail.com>
2020-12-31 22:48:59 -0800

    Solution for 1.12: anecdotes step1

---
+++ part1/anectdotes/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+
+  const getRandomNumber = (num) => {
+    return Math.floor(Math.random()*num);
+  }
+
+  return (
+    <>
+      <div>{props.anecdotes[selected]}</div>
+      <Button handleClick={() => setSelected(getRandomNumber(anecdotes.length))} text="next anectdote" />
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+);
