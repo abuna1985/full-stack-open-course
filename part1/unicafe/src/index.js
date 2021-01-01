d2e94df4537b2eaeda61d496a83efaa9dd8ccc6d
adamjabundis@gmail.com <adamjabundis@gmail.com>
2020-12-31 22:00:09 -0800

    Solution 1.7: unicafe step2, 1.8: unicafe step3, 1.9: unicafe step4, 1.10: unicafe step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,55 +5,66 @@ const Title = ({text}) => <h1>{text}</h1>
 
 const Display = ({value, text}) => <div>{text}: {value}</div>
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Button = ({handleClick, text}}) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 );
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
   const getTotal = (goodVotes, neutralVotes, badVotes) => {
     return goodVotes + neutralVotes + badVotes;
   }
 
   const getAverage = (goodVotes, neutralVotes, badVotes) => {
     let totalVotes = getTotal(goodVotes, neutralVotes, badVotes);
-    if (totalVotes === 0) {
-      return 0;
-    }
     return (goodVotes + (-badVotes))/totalVotes;
   }
 
   const getPercentage = (goodVotes, neutralVotes, badVotes) => {
     let totalVotes = getTotal(goodVotes, neutralVotes, badVotes);
-    if (totalVotes === 0) {
-      return 0;
-    }
     return goodVotes/totalVotes * 100 + '%';
   }
 
+  if (!good && !bad && !neutral) {
+    return (
+      <>
+        <Title text="Statistics" />
+        <p>No feedback given</p>
+      </>
+    );
+  }
 
+  
+  return (
+      <>
+        <Title text="Statistics" />
+        <Display value={good} text="Good" />
+        <Display value={neutral} text="Neutral" />
+        <Display value={bad} text="Bad" />
+        <Display value={getTotal(good, neutral, bad)} text="All" />
+        <Display value={getAverage(good, neutral, bad)} text="Average" />
+        <Display value={getPercentage(good, neutral, bad)} text="Positive" />
+      </>
+  );
 
- 
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
   return (
     <div>
       <Title text="Give Feedback" />
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Title text="Statistics" />
-      <Display value={good} text="Good" />
-      <Display value={neutral} text="Neutral" />
-      <Display value={bad} text="Bad" />
-      <Display value={getTotal(good, neutral, bad)} text="All" />
-      <Display value={getAverage(good, neutral, bad)} text="Average" />
-      <Display value={getPercentage(good, neutral, bad)} text="Positive" />
+      <Statistics good={good} neutral={neutral} bad ={bad} />
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />, 
