a853c6f66d1db8deb7b2af548832d9e59839f920
adamjabundis@gmail.com <adamjabundis@gmail.com>
2021-01-02 17:53:08 -0800

    Solution for 1.14: anecdotes step3

--- part1/anectdotes/src/index.js
+++ part1/anectdotes/src/index.js
@@ -10,37 +10,57 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
 
+const Title = ({text}) => <h1>{text}</h1>;
+
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
   </button>
 );
 
-const App = (props) => {
+const PopularQuote = ({quote}) => {
+  if(!quote) {
+    return <p>No votes counted yet!</p>
+  }
+
+  return <p>{quote}</p>
+}
+
+const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0);
   const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+  const [popularQuote, setPopularQuote] = useState("");
 
   const getRandomNumber = (num) => {
     return Math.floor(Math.random()*num);
   }
+
   const handleButtonClick = () => {
     return setSelected(getRandomNumber(anecdotes.length))
   }
 
   const handleVoteClick = () => {
     const copy = [...points];
-    copy[selected]++;
-    return setPoints(copy);
+
+    const getMaxPoint = (num) => {
+      return num === Math.max(...copy)
+    }
+
+    copy[selected] += 1;
+    setPoints(copy);
+    // tie goes to the index closer to 0
+    setPopularQuote(anecdotes[copy.findIndex(getMaxPoint)]);
   }
 
-
-
   return (
-    <>
-      <div>{props.anecdotes[selected]}</div>
+    <div>
+      <Title text="Anecdote of the day" />
+      <p>{anecdotes[selected]}</p>
       <Button handleClick={handleVoteClick} text="vote" />
-      <Button handleClick={handleButtonClick} text="next anectdote" />
-    </>
+      <Button handleClick={handleButtonClick} text="next anecdote" />
+      <Title text="Anecdote with the most votes" />
+      <PopularQuote quote={popularQuote} />
+    </div>
   );
 }
 
